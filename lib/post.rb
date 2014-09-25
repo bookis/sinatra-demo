c73d6a252f8621ec8eab7250899ad7abbf162f75
Bookis Smuin <bookis.smuin@gmail.com>
2014-09-25 10:31:45 -0700

    add attributes to Post

--- lib/post.rb
+++ lib/post.rb
@@ -6,9 +6,11 @@ class Post
   # Create an array of Post objects
   # with the attributes set
   # Post.all => [post, post]
+  attr_accessor :title, :url
 
   def initialize(array)
-
+    @title = array[-1].split(".")[0]
+    @url   = "/posts/#{@title}"
   end
 
   def self.all
