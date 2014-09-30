83404314c1e2d4880ff4de115b3255bd9421f4a4
Bookis Smuin <bookis.smuin@gmail.com>
2014-09-30 08:41:58 -0700

    add basic nav

--- lib/post.rb
+++ lib/post.rb
@@ -6,11 +6,9 @@ class Post
   # Create an array of Post objects
   # with the attributes set
   # Post.all => [post, post]
-  attr_accessor :title, :url
 
   def initialize(array)
-    @title = array[-1].split(".")[0]
-    @url   = "/posts/#{@title}"
+
   end
 
   def self.all
